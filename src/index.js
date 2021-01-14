0e1bf70b749588393708803d3284a43862a7bc1b
Mario Silva <mario.silvae@outlook.com>
2021-01-13 23:11:26 -0600

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./routes/App";
+import reducer from "./reducers";
 import reportWebVitals from "./reportWebVitals";
 
+const initialState = {
+  actor: "",
+  loading: false,
+  error: null,
+};
+
+const store = createStore(reducer, initialState);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
